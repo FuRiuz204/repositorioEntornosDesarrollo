bed2b587e4772e87f7297d708953d0e7c3679062
rafael <rafaeldaw1@gmail.com>
2019-04-04 09:10:58 +0200

    version 1.1 clase dado

--- Dado.java
+++ Dado.java
@@ -1,5 +1,15 @@
 class Dado{
-private int valor;
-public void tirarDado(){
-//Vacio
+	private int valor;
+	public Dado(){
+	}
+
+	public int tirarDado(){
+		//Devuelve un valor aleatorio al dado entre el 1 y el 6
+		this.valor=(int)((Math.random()*6)+1);
+		return this.valor;
+	}
+	public int getValor(){
+		return this.valor;
+	}
 }
+
